21e72b112997eb4d57ec89da5edd2e3fb3f5a0c4
Vadhir <vadhirbhupathi@gmail.com>
2016-03-28 21:46:23 +0530

    Added database stuff

--- src/components/app.js
+++ src/components/app.js
@@ -1,34 +1,64 @@
 import React from 'react';
 import TodosList from './todos-list';
-import CreateTodo from './create-todo'
+import CreateTodo from './create-todo';
+// import Firebase from 'firebase';
+import {loginWithAuth} from '../network/auth';
+// import {createUser} from '../network/auth';
+import {firebaseRef} from '../network/auth'
 
-const todos = [
-  {
-    task: 'Learn React',
-    isComplete: true
-  }
+var user = {
+  id: "vadhirbhupathi@gmail.com",
+  pass: "pakalupapito"
+};
+
+var todos = [
+
 ];
 
 export default class App extends React.Component{
   constructor(props){
     super(props);
-
     this.state = {
       todos
     }
   }
 
+  componentWillMount() {
+    loginWithAuth(user);
+    this.itemsRef=firebaseRef.child("items");
+    this.itemsRef.on("child_added", dataSnapshot => {
+      this.state.todos.push(dataSnapshot.val());
+      this.setState({
+        todos: this.state.todos
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    firebaseRef.off();
+  }
+
   createTask(task){
-    this.state.todos.push({
+    {/*this.state.todos.push({
       task,
       isComplete: false,
     });
-    this.setState({todos: this.state.todos});
+    this.setState({todos: this.state.todos});*/}
+    this.itemsRef.push({
+      task,
+      isComplete: false,
+    });
   }
 
   toggleTask(task){
     const foundTodo = _.find(this.state.todos, todo => todo.task === task);
     foundTodo.isComplete = !foundTodo.isComplete;
+    this.itemsRef.once("value", snapshot => {
+      snapshot.forEach(childSnapshot => {
+        if(childSnapshot.child('task').val()===task)
+          childSnapshot.child('isComplete').ref().set(foundTodo.isComplete);
+      });
+    });
     this.setState({todos: this.state.todos});
   }
 
@@ -51,12 +81,24 @@ export default class App extends React.Component{
   }
 
   saveTask(oldTask,newTask){
+    this.itemsRef.once("value", snapshot => {
+      snapshot.forEach(childSnapshot => {
+        if(childSnapshot.child('task').val()===oldTask)
+          childSnapshot.child('task').ref().set(newTask);
+      });
+    });
     const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
     foundTodo.task = newTask;
     this.setState({todos: this.state.todos});
   }
 
   deleteTask(taskToDelete){
+    this.itemsRef.once("value", snapshot => {
+      snapshot.forEach(childSnapshot => {
+        if(childSnapshot.child('task').val()===taskToDelete)
+          childSnapshot.ref().remove();
+      });
+    });
     _.remove(this.state.todos,todo=>todo.task===taskToDelete);
     this.setState({todos: this.state.todos});
   }
